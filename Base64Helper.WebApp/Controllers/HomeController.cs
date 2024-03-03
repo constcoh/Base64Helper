57c0c882f63f6f2924a0dcde2ce643412a7be8a7
constcoh <constcoh@gmail.com>
2024-03-03 03:03:31 +0300

    add web version

---
+++ Base64Helper.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Base64Helper.WebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Base64Helper.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
